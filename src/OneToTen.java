fc57831d02f831e942deda0cc10bb10c14b84aa9
prakash <prakasharyansingh05@gmail.com>
2025-08-14 09:44:35 +0530

    some line add and some line remove

--- src/OneToTen.java
+++ src/OneToTen.java
@@ -1,9 +1,11 @@
 public class OneToTen {
     public static void main(String[] args){
         int n = 10;
+        ///  some line;
         for(int i = 0; i<=n; i++){
             System.out.print(i+" ");
         }
-        System.out.println();
+        System.out.println(" Thank you for using Java!");
+
     }
 }
