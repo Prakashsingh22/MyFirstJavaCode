e3d247493d60a25c783c86851825a12a29494ad4
prakash <prakasharyansingh05@gmail.com>
2025-08-14 09:18:44 +0530

    just trying to check

---
+++ src/OneToTen.java
@@ -0,0 +1,9 @@
+public class OneToTen {
+    public static void main(String[] args){
+        int n = 10;
+        for(int i = 0; i<=n; i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+}
